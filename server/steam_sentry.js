2079ad8d955153be8f879a80bfb00be1d990bb78
Ar1u <mroario@t-online.de>
2017-01-05 19:42:30 +0100

    create sentrys the easy way

---
+++ server/steam_sentry.js
@@ -0,0 +1,59 @@
+var username;
+var password;
+var steamCode;
+
+var Steam = require("steam");
+var fs = require("fs");
+var readline = require("readline");
+
+var steam = new Steam.SteamClient();
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Username: ", function(answer) {
+    username = answer;
+    rl.question("Password: ", function(answer2) {
+        password = answer2;
+        rl.pause();
+        steam.logOn({
+            accountName: username,
+            password: password
+        });
+    });
+});
+
+steam.on("loggedOn", function(result) {
+    console.log("Logged in");
+    steam.setPersonaState(Steam.EPersonaState.Online);
+    setTimeout (function() {
+        process.exit();
+    }, 1000);
+});
+
+steam.on("error", function(error) {
+    if (error.cause == "logonFail") {
+        if (error.eresult == 63) {
+            rl.resume();
+            rl.question("Steam guard code: ", function(answer) {
+                steamCode = answer;
+                rl.close();
+                steam.logOn({
+                    accountName: username,
+                    password: password,
+                    authCode: steamCode
+                });
+            });
+        } else {
+            console.log("Logon fail: " + error.eresult);
+        };
+    };
+});
+
+steam.on('sentry', function(data) {
+    var format = username + ".sentry";
+    fs.writeFileSync(format, data);
+    console.log("Sentry file successfully saved!");
+});
